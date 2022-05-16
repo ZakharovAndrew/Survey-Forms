710cf7b7427ccfbff35edfcd9b809126ad01bbdd
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-05-16 11:08:37 +0300

    Update SurveyForms.js

--- src/SurveyForms.js
+++ src/SurveyForms.js
@@ -15,4 +15,29 @@ var defaultSettings = {
 };
 
 function SurveyForms(params) {
+    let settings;
+    let el;
+    let cnt = 0;
+    
+    /**
+     * Init Form
+     */
+    function init() {
+        this.settings = loadSettings(params);
+        
+        if (!document.getElementById(this.settings.id)) {
+            throw "Element not found!";
+        }
+        
+        this.el = document.getElementById(this.settings.id);       
+        
+        console.log(this.settings, this.el);
+    }
+    
+    function loadSettings(params) {
+        return Object.assign(defaultSettings , params);
+    }
+    
+    // init Form
+    init();
 }
